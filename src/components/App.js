ad60e24307b69746fa7db17a465780e019d5aa73
wangwan <wan.wang@kaiostech.com>
2018-03-13 19:32:08 +0800

    mapDispatchToProps

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import { addRecipe, removeFromCalendar } from '../actions';
 
 class App extends Component {
 
@@ -26,4 +27,11 @@ function mapStateToProps(calendar) {
     }))
   }
 }
-export default connect(mapStateToProps)(App);
+
+function mapDispatchToProps(dispatch) {
+  return {
+    selectRecipe: (data) => dispatch(addRecipe(data)),
+    remove: (data) => dispatch(removeFromCalendar(data))
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(App);
