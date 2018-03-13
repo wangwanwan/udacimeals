001d2da55ae763bf5da8c750bfd4bed909815a61
wangwan <wan.wang@kaiostech.com>
2018-03-13 20:10:10 +0800

    add food reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -38,6 +38,19 @@ const initialCalendarState = {
   }
 }
 
+function food(state={}, action) {
+  switch (action.type) {
+    case ADD_RECIPE:
+      const { recipe } = action;
+      return {
+        ...state,
+        [recipe.label]: recipe
+      }
+    default:
+      return state;
+  }
+}
+
 function calendar(state = initialCalendarState, action) {
   const { day, recipe, meal } = action;
   switch (action.type) {
